a8a13af0f2eb4f47bd20478f78b18ab14665b610
Tripta Gupta <gupta.tripta@gmail.com>
2014-04-20 22:58:51 -0700

    addressbook wip

---
+++ AddressBook/AddressBook/AddressBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by Tripta Gupta on 4/20/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
